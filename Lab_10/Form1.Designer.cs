970040fefd7293091bf883c11adb80fd0000cd5a
Dan Omelyanyuk <89798732+ItIsDan@users.noreply.github.com>
2024-05-10 21:18:13 +0700

    lab10

---
+++ Lab_10/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.results = new System.Windows.Forms.DataGridView();
+            this.Tour = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Match = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.teamBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.results)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(1, 37);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.table);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.results);
+            this.splitContainer1.Size = new System.Drawing.Size(980, 463);
+            this.splitContainer1.SplitterDistance = 459;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.table.Name = "table";
+            this.table.RowHeadersWidth = 62;
+            this.table.RowTemplate.Height = 28;
+            this.table.Size = new System.Drawing.Size(459, 463);
+            this.table.TabIndex = 0;
+            this.table.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.table_CellContentClick);
+            // 
+            // results
+            // 
+            this.results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.results.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tour,
+            this.Match,
+            this.Result});
+            this.results.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.results.Location = new System.Drawing.Point(0, 0);
+            this.results.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.results.Name = "results";
+            this.results.RowHeadersWidth = 62;
+            this.results.RowTemplate.Height = 28;
+            this.results.Size = new System.Drawing.Size(518, 463);
+            this.results.TabIndex = 0;
+            // 
+            // Tour
+            // 
+            this.Tour.HeaderText = "Тур";
+            this.Tour.MinimumWidth = 8;
+            this.Tour.Name = "Tour";
+            this.Tour.Width = 150;
+            // 
+            // Match
+            // 
+            this.Match.HeaderText = "Матч";
+            this.Match.MinimumWidth = 8;
+            this.Match.Name = "Match";
+            this.Match.Width = 150;
+            // 
+            // Result
+            // 
+            this.Result.HeaderText = "Результат матча";
+            this.Result.MinimumWidth = 8;
+            this.Result.Name = "Result";
+            this.Result.Width = 150;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(195, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Таблица";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(668, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Результаты матчей";
+            // 
+            // teamBindingSource
+            // 
+            this.teamBindingSource.DataSource = typeof(Lab_10.Form1.Team);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(9, 8);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Play!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.results)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView results;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tour;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Match;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.BindingSource teamBindingSource;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab_10/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab_10
+{
+    public partial class Form1 : Form
+    {
+        public List<Team> teams;
+        public List<GameObject> games = new List<GameObject>();
+        Random rand = new Random();
+        public int amount = 8;
+        public List<DataGridViewRow> rows = new List<DataGridViewRow>();
+
+        public Form1()
+        {   
+            InitializeComponent();
+
+            teams = new List<Team>
+            {
+                new Team("Chelsea", 0, 1.5),
+                new Team("Arsenal", 0, 1.25),
+                new Team("Liverpool", 0, 1.25),
+                new Team("Man City", 0, 1.2),
+                new Team("Man Utd", 0, 1),
+                new Team("Everton", 0, 0.95),
+                new Team("Stoke", 0, 0.9),
+                new Team("Hull", 0, 0.9)
+            };
+            table.DataSource = teams;
+        }
+
+        public class Team
+        {
+            public string Name { get; set; }
+            public int Points { get; set; }
+            private double lambda { get; set; }
+
+            public double getLambda()
+            {
+                return this.lambda;
+            }
+
+            public Team(string Name, int Points, double lambda)
+            {
+                this.Name = Name;
+                this.Points = Points;
+                this.lambda = lambda;
+            }
+        }
+
+        public class GameObject
+        {
+            public Team team1 { get; set; }
+            public Team team2 { get; set; }
+            public int team1Score { get; set; }
+            public int team2Score { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < amount; i++)
+            {
+                for(int j = i + 1; j < amount; j++)
+                {
+                    games.Add(new GameObject
+                    {
+                        team1 = teams[i],
+                        team2 = teams[j]
+                    });
+                }
+            }
+
+            for (int i = 0; i < 28; i++) // 7 + 6 + 5 + 4 + 3 + 2 + 1
+            {
+                double a = rand.NextDouble();
+                double lambda1 = games[i].team1.getLambda();
+                double sum = 0;
+                int m = 0;
+
+                while (sum > lambda1 * (-1)) { // Датчик Пуассона
+                    sum += Math.Log(a);
+                    m++;
+                }
+                games[i].team1Score = m;
+
+                sum = 0;
+                m = 0;
+                a = rand.NextDouble();
+
+                double lambda2 = games[i].team2.getLambda();
+
+                while (sum > lambda2 * (-1)) { // Датчик Пуассона
+                    sum += Math.Log(a);
+                    m++;
+                }
+                games[i].team2Score = m; // x = m;
+
+                if (games[i].team1Score > games[i].team2Score)
+                {
+                    games[i].team1.Points += 3;
+                    continue;
+                }
+
+                else if (games[i].team1Score < games[i].team2Score) { 
+                    games[i].team2.Points += 3;
+                    continue;
+                }
+
+                else
+                {
+                    games[i].team1.Points += 1;
+                    games[i].team2.Points += 1;
+                    continue;
+                }
+            }
+
+            table.Refresh();
+            
+            List<Team> SortedList = teams.OrderByDescending(o => o.Points).ToList();
+
+            table.DataSource = SortedList;
+           
+            for(int i = 0; i < games.Count; i++)
+                results.Rows.Add("1", games[i].team1.Name + " vs " + games[i].team2.Name, games[i].team1Score + " - " + games[i].team2Score);
+
+            int count = 0;
+            for(int i = 0; i < amount - 1; i++)
+                for(int j = i; j < amount - 1; j++)
+                {
+                    results.Rows[count].Cells[0].Value = j + 1;
+                    count++;
+                }
+        }
+
+        private void table_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
